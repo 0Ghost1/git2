6fa4d5aadf69b20450fd6637879eed5b4a0c17d3
JDH <chebakov2009@gmail.com>
2025-03-23 02:45:14 +0300

    Реклама с картинкой

--- server.py
+++ server.py
@@ -46,5 +46,42 @@ def image_mars():
                 </html>'''
 
 
+@app.route('/promotion_image')
+def advertising():
+    return '''<!doctype html>
+                <html lang="en">
+                <head>
+                    <meta charset="utf-8">
+                    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+                    <link rel="stylesheet"
+                          href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css"
+                          integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1"
+                          crossorigin="anonymous">
+                    <title>Колонизация/title>
+                </head>
+                <body>
+                <h1>Жди нас, Марс!</h1>
+                <img src="/static/image/mars.jpeg" alt="Марс закрыт для зрителей">
+                
+                <div class="alert" style="background-color: #dcdcdc; font-weight: bold;">
+                    Человечество вырастает из детства.
+                </div>
+                <div class="alert" style="background-color: #c8e6c9; font-weight: bold;">
+                    Человечеству мала одна планета.
+                </div>
+                <div class="alert" style="background-color: #e3f2fd; font-weight: bold;">
+                    Мы сделаем обитаемыми безжизненные пока планеты.
+                </div>
+                <div class="alert" style="background-color: #fff9c4; font-weight: bold;">
+                    И начнем с Марса!
+                </div>
+                <div class="alert" style="background-color: #f8bbd0; font-weight: bold;">
+                    Присоединяйся!
+                </div>
+                </body>
+                
+                </html>'''
+
+
 if __name__ == '__main__':
     app.run(port=8080, host='127.0.0.1')
